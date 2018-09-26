95452b389f55c84ba2d183a4807d484491df2c66
Matthew <Matthew@DESKTOP-FBBVVQH>
2018-09-26 09:20:21 -0400

    Final Program

--- Palindromic Product/Program.cs
+++ Palindromic Product/Program.cs
@@ -10,6 +10,49 @@ namespace Palindromic_Product
     {
         static void Main(string[] args)
         {
+            int num1 = 999;
+            int num2 = 999;
+            string tempVar = "";
+            string tempVar2 = "";
+            List<int> productsList = new List<int>();
+
+            for (int i = 1; i < 100000; i++)
+            {
+                tempVar2 = "";
+
+                var product = num1 * num2;
+
+                tempVar = Convert.ToString(product);
+
+                char[] varChar = tempVar.ToCharArray();
+
+                for (int j = varChar.Length - 1; j >= 0; j--)
+                {
+                    tempVar2 += tempVar[j];
+                }
+                if (Convert.ToString(product) == tempVar2)
+                {
+                    Console.WriteLine(num1 + " X " + num2 + " = " + product);
+                    productsList.Add(product);
+                }
+
+                num2--;
+
+                if (num2 == 0)
+                {
+                    num2 = 999;
+                    num1--;
+                }
+                if (num1 == 0)
+                {
+                    break;
+                }
+            }
+            productsList.Sort();
+            {
+                Console.WriteLine(productsList[productsList.Count - 1]);
+            }
+
         }
     }
 }
